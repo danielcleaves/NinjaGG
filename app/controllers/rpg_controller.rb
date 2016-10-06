fa6685da0733ffa8ee2467f82c3b6a8046d8424a
daniel <daniel_cleaves@hotmail.com>
2016-10-06 08:44:45 -0500

    first add

---
+++ app/controllers/rpg_controller.rb
@@ -0,0 +1,53 @@
+class RpgController < ApplicationController
+  def index
+    if session[:gold] == nil
+      session[:gold] = 0
+    end
+
+    if session[:messages] == nil
+        session[:messages] = []
+    end
+
+    session[:total_golds] = session[:gold].to_i + session[:total_golds].to_i
+    @total_golds = session[:total_golds]
+    @current_date_time = Time.new
+    @current_hour = @current_date_time.hour
+        if(@current_hour > 11)
+          @hour = @current_hour - 12
+
+          if(@hour == 0)
+            @date_time = "(#{@current_date_time.year}/#{@current_date_time.month}/#{@current_date_time.day} 12:#{@current_date_time.min} pm)"
+          else
+            @date_time = "(#{@current_date_time.year}/#{@current_date_time.month}/#{@current_date_time.day} #{@hour}:#{@current_date_time.min} pm)"
+          end
+      else
+          @date_time = "(#{@current_date_time.year}/#{@current_date_time.month}/#{@current_date_time.day} #{@current_hour}:#{@current_date_time.min} am)"
+      end
+
+      if session[:gold] < 0
+          session[:messages] << "Entered a casino and lost #{session[:gold]*-1} golds... Ouch.. #{@date_time}"
+      elsif session[:gold] > 0
+          session[:messages] << "Earned #{session[:gold]} golds from the #{session[:building]}! .. #{@date_time}"
+      end
+
+      @activities = session[:messages]
+      session[:gold] = 0
+      session[:building] = ''
+    end
+
+  def new
+    if params[:building] == 'farm'
+        session[:gold] = rand(10..20)
+    elsif params[:building] == 'cave'
+        session[:gold] = rand(5..10)
+    elsif params[:building] == 'house'
+        session[:gold] = rand(2..5)
+    elsif params[:building] == 'casino'
+        session[:gold] = rand(-50..50)  
+    end
+
+      session[:building] == params[:building]
+
+    redirect_to action: "index"
+  end
+end
